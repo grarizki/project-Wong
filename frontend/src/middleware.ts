83d60eec5b9e47a9e57bbf3db910b291445f1678
grarizki <28901232+grarizki@users.noreply.github.com>
2023-05-30 21:20:12 +0700

    refactor if and type

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -19,19 +19,19 @@ function getLocale(request: NextRequest): string | undefined {
 }
 
 export function middleware(request: NextRequest) {
-    const pathname = request.nextUrl.pathname;
+    const { pathname } = request.nextUrl;
 
     // // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
     // // If you have one
     if (
-        [
-            '/manifest.json',
-            '/favicon.ico',
-            // Your other files in `public`
-        ].includes(pathname)
-    )
-        return;
-
+      [
+        '/manifest.json',
+        '/favicon.ico',
+        // Your other files in `public`
+      ].includes(pathname)
+    ) {
+      return;
+    }
     // Check if there is any supported locale in the pathname
     const pathnameIsMissingLocale = i18n.locales.every(
         (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
